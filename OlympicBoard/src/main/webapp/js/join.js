417bbe421f28439eabe15997f0447cb9d697ad83
318 <318@DESKTOP-992VT3E>
2022-01-27 16:05:59 +0900

    up

--- OlympicBoard/src/main/webapp/js/join.js
+++ OlympicBoard/src/main/webapp/js/join.js
@@ -4,18 +4,18 @@ function onBlurFn(obj){
 	var id = obj.id;
 	var reg = "";
 	var passValue = document.getElementById("pass").value;
-	var ph1val = document.getElementById("phone1").value;
-	var ph2val = document.getElementById("phone2").value;
-	var ph3val = document.getElementById("phone3").value;
-	var ph1reg = /\d{2,3}/;
-	var ph2reg = /\d{3,4}/;
-	var ph3reg = /\d{4}/;
-	var bir1val = document.getElementById("birth1").value;
-	var bir2val = document.getElementById("birth2").value;
-	var bir3val = document.getElementById("birth3").value;
-	var bir1reg = /\d{4}/;
-	var bir2reg = /\d{1,2}/;
-	var bir3reg = /\d{1,2}/;
+	var ph1Val = document.getElementById("phone1").value;
+	var ph2Val = document.getElementById("phone2").value;
+	var ph3Val = document.getElementById("phone3").value;
+	var ph1Reg = /\d{2,3}/;
+	var ph2Reg = /\d{3,4}/;
+	var ph3Reg = /\d{4}/;
+	var bir1Val = document.getElementById("birth1").value;
+	var bir2Val = document.getElementById("birth2").value;
+	var bir3Val = document.getElementById("birth3").value;
+	var bir1Reg = /\d{4}/;
+	var bir2Reg = /\d{1,2}/;
+	var bir3Reg = /\d{1,2}/;
 
 	if(id =="id"){			
 		reg = /^[a-z]+[a-z0-9]{5,19}$/g;
@@ -108,7 +108,7 @@ function onBlurFn(obj){
 	}
 
 	if(id =="phone1"){
-		if(ph1reg.test(ph1val) && ph2reg.test(ph2val) && ph3reg.test(ph3val)){
+		if(ph1Reg.test(ph1Val) && ph2Reg.test(ph2Val) && ph3Reg.test(ph3Val)){
 			span.style.visibility = "visible";
 			span.textContent = "올바르게 입력하셨습니다";
 			span.style.color = "green";
@@ -119,7 +119,7 @@ function onBlurFn(obj){
 		}
 	}
 	if(id =="phone2"){
-		if(ph1reg.test(ph1val) && ph2reg.test(ph2val) && ph3reg.test(ph3val)){
+		if(ph1Reg.test(ph1Val) && ph2Reg.test(ph2Val) && ph3Reg.test(ph3Val)){
 			span.style.visibility = "visible";
 			span.textContent = "올바르게 입력하셨습니다";
 			span.style.color = "green";
@@ -130,7 +130,7 @@ function onBlurFn(obj){
 		}
 	}
 	if(id =="phone3"){
-		if(ph1reg.test(ph1val) && ph2reg.test(ph2val) && ph3reg.test(ph3val)){
+		if(ph1Reg.test(ph1Val) && ph2Reg.test(ph2Val) && ph3Reg.test(ph3Val)){
 			span.style.visibility = "visible";
 			span.textContent = "올바르게 입력하셨습니다";
 			span.style.color = "green";
@@ -142,9 +142,9 @@ function onBlurFn(obj){
 	}
 
 	if(id =="birth1"){			
-		if(bir1reg.test(bir1val) && bir2reg.test(bir2val) && bir3reg.test(bir3val) 
-		&& bir1val!="0000" && bir2val!="0" && bir2val!="00" && bir3val!="0" && bir3val!="00" 
-		&& parseInt(bir2val)<=12 && parseInt(bir3val)<=31){
+		if(bir1Reg.test(bir1Val) && bir2Reg.test(bir2Val) && bir3Reg.test(bir3Val) 
+		&& bir1Val!="0000" && bir2Val!="0" && bir2Val!="00" && bir3Val!="0" && bir3Val!="00" 
+		&& parseInt(bir2Val)<=12 && parseInt(bir3Val)<=31){
 			span.style.visibility = "visible";
 			span.textContent = "올바르게 입력하셨습니다";
 			span.style.color = "green";
@@ -159,9 +159,9 @@ function onBlurFn(obj){
 		}
 	}
 	if(id =="birth2"){			
-		if(bir1reg.test(bir1val) && bir2reg.test(bir2val) && bir3reg.test(bir3val) 
-			&& bir1val!="0000" && bir2val!="0" && bir2val!="00" && bir3val!="0" && bir3val!="00" 
-			&& parseInt(bir2val)<=12 && parseInt(bir3val)<=31){
+		if(bir1Reg.test(bir1Val) && bir2Reg.test(bir2Val) && bir3Reg.test(bir3Val) 
+		&& bir1Val!="0000" && bir2Val!="0" && bir2Val!="00" && bir3Val!="0" && bir3Val!="00" 
+		&& parseInt(bir2Val)<=12 && parseInt(bir3Val)<=31){
 			span.style.visibility = "visible";
 			span.textContent = "올바르게 입력하셨습니다";
 			span.style.color = "green";
@@ -171,7 +171,7 @@ function onBlurFn(obj){
 			span.style.color = "red";
 		}else if(parseInt(value)>12){
 			span.style.visibility = "visible";
-			span.textContent = "출생월은 12이하로 입력하세요";
+			span.textContent = "출생월은 12 이하로 입력하세요";
 			span.style.color = "red";
 		}else{
 			span.style.visibility = "visible";
@@ -180,9 +180,9 @@ function onBlurFn(obj){
 		}
 	}
 	if(id =="birth3"){
-		if(bir1reg.test(bir1val) && bir2reg.test(bir2val) && bir3reg.test(bir3val) 
-			&& bir1val!="0000" && bir2val!="0" && bir2val!="00" && bir3val!="0" && bir3val!="00" 
-			&& parseInt(bir2val)<=12 && parseInt(bir3val)<=31){
+		if(bir1Reg.test(bir1Val) && bir2Reg.test(bir2Val) && bir3Reg.test(bir3Val) 
+		&& bir1Val!="0000" && bir2Val!="0" && bir2Val!="00" && bir3Val!="0" && bir3Val!="00" 
+		&& parseInt(bir2Val)<=12 && parseInt(bir3Val)<=31){
 			span.style.visibility = "visible";
 			span.textContent = "올바르게 입력하셨습니다";
 			span.style.color = "green";
@@ -192,7 +192,7 @@ function onBlurFn(obj){
 			span.style.color = "red";
 		}else if(parseInt(value)>31){
 			span.style.visibility = "visible";
-			span.textContent = "출생일은 31이하로 입력하세요";
+			span.textContent = "출생일은 31 이하로 입력하세요";
 			span.style.color = "red";
 		}else{
 			span.style.visibility = "visible";
@@ -203,7 +203,152 @@ function onBlurFn(obj){
 }
 
 function joinSubmitFn(){
-	document.joinFrm.method = "post";
-	document.joinFrm.action = "joinOk.jsp";
-	document.joinFrm.submit();
+	var result = true;
+
+	var value = document.getElementById("id").value;
+	var span = document.getElementById("idSpan");
+	var reg = /^[a-z]+[a-z0-9]{5,19}$/g;
+	if(value == ""){
+		span.style.visibility = "visible";
+		span.textContent = "아이디를 입력하세요";
+		span.style.color = "red";
+		result = false;
+	}else if(!reg.test(value)){
+		span.style.visibility = "visible";
+		span.textContent = "영문으로 시작하는 6~20자리의 영문or숫자만 가능합니다";
+		span.style.color = "red";
+		result = false;
+	}else{
+		span.style.visibility = "hidden";
+		span.textContent = "";
+	}
+
+	value = document.getElementById("pass").value;
+	span = document.getElementById("passSpan");
+	reg = /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{6,20}$/;
+	if(value == ""){
+		span.style.visibility = "visible";
+		span.textContent = "비밀번호를 입력하세요";
+		span.style.color = "red";
+		result = false;
+	}else if(!reg.test(value)){
+		span.style.visibility = "visible";
+		span.textContent = "영문, 숫자, 특수문자를 모두 포함한 6~20자리만 가능합니다";
+		span.style.color = "red";
+		result = false;
+	}else{
+		span.style.visibility = "hidden";
+		span.textContent = "";
+	}
+
+	value = document.getElementById("passcheck").value;
+	span = document.getElementById("passcheckSpan");
+	passValue = document.getElementById("pass").value;
+	if(value == ""){
+		span.style.visibility = "visible";
+		span.textContent = "비밀번호를 다시 입력하세요";
+		span.style.color = "red";
+		result = false;
+	}else if(value != passValue){
+		span.style.visibility = "visible";
+		span.textContent = "비밀번호가 다릅니다";
+		span.style.color = "red";
+		result = false;
+	}else{
+		span.style.visibility = "hidden";
+		span.textContent = "";
+	}
+
+	value = document.getElementById("name").value;
+	span = document.getElementById("nameSpan");
+	reg = reg = /[가-힣]{2,20}$/;
+	if(value == ""){
+		span.style.visibility = "visible";
+		span.textContent = "이름을 입력하세요";
+		span.style.color = "red";
+		result = false;
+	}else if(!reg.test(value)){
+		span.style.visibility = "visible";
+		span.textContent = "한글만 입력 가능합니다";
+		span.style.color = "red";
+		result = false;
+	}else{
+		span.style.visibility = "hidden";
+		span.textContent = "";
+	}
+
+	value = document.getElementById("email").value;
+	span = document.getElementById("emailSpan");
+	reg = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+	if(value == ""){
+		span.style.visibility = "visible";
+		span.textContent = "이메일 주소를 입력하세요";
+		span.style.color = "red";
+		result = false;
+	}else if(!reg.test(value)){
+		span.style.visibility = "visible";
+		span.textContent = "이메일 주소 형식이 올바르지 않습니다";
+		span.style.color = "red";
+		result = false;
+	}else{
+		span.style.visibility = "hidden";
+		span.textContent = "";
+	}
+
+	span = document.getElementById("phoneSpan");
+	var ph1Val = document.getElementById("phone1").value;
+	var ph2Val = document.getElementById("phone2").value;
+	var ph3Val = document.getElementById("phone3").value;
+	var ph1Reg = /\d{2,3}/;
+	var ph2Reg = /\d{3,4}/;
+	var ph3Reg = /\d{4}/;		
+	if(ph1Reg.test(ph1Val) && ph2Reg.test(ph2Val) && ph3Reg.test(ph3Val)){
+		span.style.visibility = "hidden";
+		span.textContent = "";
+	}else{
+		span.style.visibility = "visible";
+		span.textContent = "연락처를 올바르게 입력해주세요";
+		span.style.color = "red";
+		result = false;
+	}
+
+	span = document.getElementById("birthSpan");
+	var bir1Val = document.getElementById("birth1").value;
+	var bir2Val = document.getElementById("birth2").value;
+	var bir3Val = document.getElementById("birth3").value;
+	var bir1Reg = /\d{4}/;
+	var bir2Reg = /\d{1,2}/;
+	var bir3Reg = /\d{1,2}/;
+	if(bir1Reg.test(bir1Val) && bir2Reg.test(bir2Val) && bir3Reg.test(bir3Val) 
+		&& bir1Val!="0000" && bir2Val!="0" && bir2Val!="00" && bir3Val!="0" && bir3Val!="00" 
+		&& parseInt(bir2Val)<=12 && parseInt(bir3Val)<=31){
+		span.style.visibility = "hidden";
+		span.textContent = "";
+	}else if(bir1Val=="0000" || bir2Val=="0" || bir2Val=="00" || bir3Val=="0" || bir3Val=="00"){
+		span.style.visibility = "visible";
+		span.textContent = "0은 입력하실 수 없습니다";
+		span.style.color = "red";
+		result = false;
+	}else if(parseInt(bir2Val)>12){
+		span.style.visibility = "visible";
+		span.textContent = "출생월은 12 이하로 입력하세요";
+		span.style.color = "red";
+		result = false;
+	}else if(parseInt(bir3Val)>31){
+		span.style.visibility = "visible";
+		span.textContent = "출생일은 31 이하로 입력하세요";
+		span.style.color = "red";
+		result = false;
+	}else{
+		span.style.visibility = "visible";
+		span.textContent = "생년월일을 올바르게 입력해주세요";
+		span.style.color = "red";
+		result = false;
+	}
+
+	if(result){
+		document.joinFrm.method = "post";
+		document.joinFrm.action = "joinOk.jsp";
+		document.joinFrm.submit();
+	}
 }
