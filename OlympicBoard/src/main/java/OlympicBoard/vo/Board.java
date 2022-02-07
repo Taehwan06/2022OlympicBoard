5f76f2b8c53aa0085e326b290b6833d364b81dc4
318 <318@DESKTOP-992VT3E>
2022-02-07 17:43:47 +0900

    up

---
+++ OlympicBoard/src/main/java/OlympicBoard/vo/Board.java
@@ -0,0 +1,84 @@
+package OlympicBoard.vo;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Board {
+	private int bidx;
+	private int midx;
+	private String bwriter;
+	private String bcontent;
+	private String bwdate;
+	private String bsubject;
+	private String bnotice;
+	private String bdelyn;
+	private int bhit;
+	private int recnt;
+	
+	
+	public int getBidx() {
+		return bidx;
+	}
+	public void setBidx(int bidx) {
+		this.bidx = bidx;
+	}
+	public int getMidx() {
+		return midx;
+	}
+	public void setMidx(int midx) {
+		this.midx = midx;
+	}
+	public String getBwriter() {
+		return bwriter;
+	}
+	public void setBwriter(String bwriter) {
+		this.bwriter = bwriter;
+	}
+	public String getBcontent() {
+		return bcontent;
+	}
+	public void setBcontent(String bcontent) {
+		this.bcontent = bcontent;
+	}
+	public String getBwdate() throws ParseException {
+		Date date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S").parse(bwdate);
+		
+		String outdate = new SimpleDateFormat("yyyy년MM월dd일 HH시mm분ss초").format(date);
+		
+		return outdate;
+	}
+	public void setBwdate(String bwdate) {
+		this.bwdate = bwdate;
+	}
+	public String getBsubject() {
+		return bsubject;
+	}
+	public void setBsubject(String bsubject) {
+		this.bsubject = bsubject;
+	}
+	public String getBnotice() {
+		return bnotice;
+	}
+	public void setBnotice(String bnotice) {
+		this.bnotice = bnotice;
+	}
+	public String getBdelyn() {
+		return bdelyn;
+	}
+	public void setBdelyn(String bdelyn) {
+		this.bdelyn = bdelyn;
+	}
+	public int getBhit() {
+		return bhit;
+	}
+	public void setBhit(int bhit) {
+		this.bhit = bhit;
+	}
+	public int getRecnt() {
+		return recnt;
+	}
+	public void setRecnt(int recnt) {
+		this.recnt = recnt;
+	}
+}
---
+++ OlympicBoard/src/main/java/OlympicBoard/vo/Reply.java
@@ -0,0 +1,63 @@
+package OlympicBoard.vo;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Reply {
+	private int ridx;
+	private int bidx;
+	private int midx;
+	private String rcontent;
+	private String rwriter;
+	private String rwdate;
+	private String rdelyn;
+	
+	
+	public String getRdelyn() {
+		return rdelyn;
+	}
+	public void setRdelyn(String rdelyn) {
+		this.rdelyn = rdelyn;
+	}
+	public int getRidx() {
+		return ridx;
+	}
+	public void setRidx(int ridx) {
+		this.ridx = ridx;
+	}
+	public int getBidx() {
+		return bidx;
+	}
+	public void setBidx(int bidx) {
+		this.bidx = bidx;
+	}
+	public int getMidx() {
+		return midx;
+	}
+	public void setMidx(int midx) {
+		this.midx = midx;
+	}
+	public String getRcontent() {
+		return rcontent;
+	}
+	public void setRcontent(String rcontent) {
+		this.rcontent = rcontent;
+	}
+	public String getRwriter() {
+		return rwriter;
+	}
+	public void setRwriter(String rwriter) {
+		this.rwriter = rwriter;
+	}
+	public String getRwdate() throws ParseException {
+		Date date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S").parse(rwdate);
+		
+		String outdate = new SimpleDateFormat("yy-MM-dd HH:mm").format(date);
+		
+		return outdate;
+	}
+	public void setRwdate(String rwdate) {
+		this.rwdate = rwdate;
+	}
+}
